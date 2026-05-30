aa6868d75d69a279b57733e4faf908ae58434839
Karen Kataoka <karen.kataoka@yahoo.com>
2026-05-30 15:13:18 -0400

    primeiro commit

---
+++ operadoresincremento.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main () {
+
+    /*
+  ....Incremento (++)
+  ....Pré-Incremento ++a
+  ....Pós-Incremento a++
+  ....Decremento (--)
+  ....Pré-Decremento --a
+  ....Pós-Decremento a--
+  */
+
+  int numero1 = 1;
+  
+ printf("Antes incremento: %d\n", numero1);
+  numero1++;
+ printf("Apos incremento: %d\n", numero1);
+
+ 
+ numero1--;
+ printf("Apos decremento: %d\n", numero1);
+
+  return 0;
+
+}
+
+
+
+
+
+ 
+
+
+
+
+
+
