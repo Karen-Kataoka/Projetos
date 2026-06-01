470e92204bc37fb618d979ccf3681859601c242a
Karen Kataoka <karen.kataoka@yahoo.com>
2026-06-01 16:04:40 -0400

    media aluno

---
+++ aluno.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (){
+
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("***Programa de Cálculo de Média***\n");
+
+    printf("Digite a sua primeira nota: \n");
+    scanf("%f", &nota1);
+
+    printf("Digite a sua Segunda nota: \n");
+    scanf("%f", &nota2);
+
+    printf("Digite a sua Terceira nota: \n");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A média é: %.1f", media);
+
+
+
+
+
+    return 0;
+
+
+}
